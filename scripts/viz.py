022afc63e360844170a854dc71c438ad4eec1522
Michael Sevilla <mikesevilla3@gmail.com>
2014-12-12 14:44:57 -0800

    More elegant vizualizer.

--- scripts/viz.py
+++ scripts/viz.py
@@ -3,88 +3,105 @@ import os, sys, subprocess, time
 
 types = ['MDSs', 'MONs', 'OSDs', 'CLIENTs']
 
-def main():
-    print "Let's graph."
-    daemons = parse_config()
-    print "Who do you want to profile?"
-    for t in types: print "-- " + str(types.index(t) + 1) + ". " + t 
-    d = raw_input(">> ")
-    if d in types:
-        graph(d, daemons)
-    else:
-        try:
-            i = int(d)
-        except:
-            whoops("Your daemon selection sucked (or you just pressed enter). I bequeath an exit upon you.\n")
-        if i < len(types):
-            graph(types[int(d) - 1], daemons)
-
 def whoops(error):
     print "Whoops. " + error
     sys.exit(0)
 
+def retry_graph(d, error):
+    print "Sorry. " + error + "\nLet's try again!\n"
+    graph(d)
+
+def retry_main(error):
+    print "Sorry. " + error + "\nLet's try again!\n"
+    main()
+
+def raw_input_exit(msg):
+    ret = raw_input(msg + ">> ")
+    if ret == 'exit':
+        print "Ok, bye!"
+        sys.exit(0)
+    return ret
+
+# run a generic command with bash
+def run(command):
+    p = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    output = p.communicate()[0]
+    if p.returncode != 0: 
+        whoops("Failed to run command: " + command)
+    return output
+
+
+# pull out the server IDs from the config file
 def parse_config():
-    if len(sys.argv) < 2: config = "/user/msevilla/ceph-deploy/config.sh"
-    else: config = sys.argv[1]
-    print "- using", config, "and the default directory\n"
-    f = open(config, 'r')
+    if len(sys.argv) < 2: 
+        config = "/user/msevilla/ceph-deploy/config.sh"
+    else: 
+        config = sys.argv[1]
+
+    # check that the directory has all the necessary folders
+    dirs = os.listdir("./")
+    if 'perf' not in dirs and 'cpu' not in dirs:
+        whoops("It looks like you aren't in a directory that has the necessary log files (e.g., ./perf, ./cpu).")
+    
+    try:
+        f = open(config, 'r')
+    except IOError:
+        whoops("Can't find the config file: " + config)
     daemons = {}
-    for d in types: daemons[d] = None
+    for d in types: 
+        daemons[d] = None
     for line in f:
         if "#" not in line:
             for d in daemons:
                 if d in line:
                     daemons[d] = line.split('=')[1].strip('\n').strip('\"')
+    f.close()
+    # make sure we have all server types
     for d in daemons:
         if daemons[d] is None:
-            f.close()
             whoops("Failed to find all daemons in the config.\n")
-    f.close()
     return daemons
 
-def run(command):
-    p = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-    output = p.communicate()[0]
-    if p.returncode != 0:
-        whoops("Failed to run command: " + command)
-    return output
+# read the config and print out which daemons are on which servers
+def config(d):
+    daemons = parse_config()
+    print d + ": " + str(daemons.get(d).split()) + "\n"
+    main()
 
-def graph(d, daemons):
-    ds = daemons.get(d).split()
-
-    tool = raw_input("Pick metric type \n-- 1. utilization\n-- 2. perfcounter \n>> ")
-    print "tool", tool, "\n"
-    if tool == 'utilization' or tool == '1':
-        files = run("ls ./cpu")
-        for f in files.split():
-            if "issdm-" in f and ds[0] in f and "tab" in f:
+# use tailplot to graph the user-selected component and values
+def graph(d):
+    daemons = parse_config().get(d).split()
+    metric = raw_input_exit("Pick metric type \n-- 1. utilization\n-- 2. perfcounter\n")
+    if metric == 'utilization' or metric == '1':
+        # pull out the file name
+        files = os.listdir("./cpu")
+        for f in files:
+            if "issdm-" in f and daemons[0] in f and "tab" in f:
                 filename = "./cpu/" + f
+        # pull out the values we can graph
         options = run("parse_collectl.py " + filename)
-    elif tool == 'perfcounter' or tool == '2':
-        options = run("parse_perf.py ./perf/" + ds[0] + "-0 component component")
-        component = raw_input("Pick component: " + str(options) + ">> ")
+    elif metric == 'perfcounter' or metric == '2':
+        # pull out the component in the perf counter file
+        options = run("parse_perf.py ./perf/" + daemons[0] + "-0 component component")
+        component = raw_input_exit("Pick component: " + str(options) + "\n")
         if component not in options:
-            print "Sorry, not one of the components\n"
-            graph(daemons)
+            retry_graph(d, "I don't know that metric.")
         print "\n"
-        filename = "./perf/" + component + "-issdm-" + ds[0] + ".timing"
-        options = run("parse_perf.py ./perf/" + ds[0] + "-0 " + component + " legend")
- 
+        filename = "./perf/" + component + "-issdm-" + daemons[0] + ".timing"
+        # pull out the values we can graph
+        options = run("parse_perf.py ./perf/" + daemons[0] + "-0 " + component + " legend")
     else:
-        print "Sorry, I don't know that tool.\n"
-        graph(daemons)
-
-    vals = raw_input("Pick value to graph: " + str(options) + "\n>> ")
+        retry_graph(d, "I don't know that metric.")
+    
+    vals = raw_input_exit("Pick value to graph: " + str(options) + "\n")
     if not vals:
-        print "Sorry, you didn't tell me what to plot.\n"
-        graph(daemons)
+        retry_graph(d, "You didn't tell me what to plot.")
     fields = vals.split()
     indices = []
     f = ""
     for v in fields:
-        if v not in options:
-            print "Sorry, not one of the plottable values.\n"
-            graph(daemons)
+        if v not in options.split():
+            retry_graph(d, "That isn't one of the plottable values.")
         indices.append(options.split().index(v) + 1)
         f += v + ","
     print "\n"
@@ -94,15 +111,43 @@ def graph(d, daemons):
         s += str(i) + ","
     s = s[0:len(s) - 1]
     f = f[0:len(f) - 1]
-    for mds in ds:
-        if tool == 'utilization' or tool == '1':
-            cmd = "tailplot -x 2 --field-format=2,date,HH:mm:ss --x-format=date,HH:mm:ss -f " + f + " -s " + s + " " + filename
-        elif tool == 'perfcounter' or tool == '2':
-            cmd = "tailplot -x 2 --field-format=2,date,HH:mm:ss --x-format=date,HH:mm:ss -f " + f + " -s " + s + " " + filename
+
+    for daemon in daemons:
+        if metric == 'utilization' or metric == '1':
+            # pull out the file name
+            files = os.listdir("./cpu")
+            for name in files:
+                if "issdm-" in name and daemon in name and "tab" in name:
+                    filename = "./cpu/" + name
+        elif metric == 'perfcounter' or metric == '2':
+            filename = "./perf/" + component + "-issdm-" + daemon + ".timing"
+        if os.path.exists(filename):
+            cmd = "tailplot -x 2 --field-format=2,date,HH:mm:ss --x-format=date,HH:mm:ss -f " + f + " -s " + s + " -t issdm-" + daemon + " " + filename
+        else:
+            whoops("Filename " + filename + " does not exists. Make sure you made during the experiment run.")
         print "Running:", cmd
         subprocess.Popen(cmd.split())
-        time.sleep(3)
     print "********** DONE **********\n\n"
+    time.sleep(3)
     main()
     
+def main():
+    action = raw_input_exit("What do you want to do (enter 'exit' at any time...)?\n-- 1. view config\n-- 2. graph\n")
+    if not action or action not in ['view config', 'graph', '1', '2']:
+        retry_main("Please select from the action list.")
+    print "\nWho do you want to see?"
+    for t in types: print "-- " + str(types.index(t) + 1) + ". " + t
+    d = raw_input_exit("")
+    print "\n"
+    if d not in types:
+        # maybe the user entered the number instead
+        try: 
+            d = types[int(d) - 1] 
+        except: 
+            retry_main("Your daemon selection sucked (or you just pressed enter).\n")
+    if action == 'view config' or action == '1':
+        config(d)
+    elif action == 'graph' or action == '2': 
+        graph(d)
+
 main()
