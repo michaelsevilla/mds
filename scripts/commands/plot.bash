c9495a08bbb1d6c7ce7716478d6253faf1f61c0e
Michael Sevilla <mikesevilla3@gmail.com>
2014-12-15 11:56:56 -0800

    Tweaked cluster commands.

--- scripts/commands/plot.bash
+++
@@ -1,6 +0,0 @@
-# To plot the reply rate
-export MDS=15
-export SAMPLES=20
-for i in $(seq 0 $SAMPLES); do parse_perf.py $MDS-$i mds reply_latency >> reply_issdm-$MDS; done
-parse_replyl.py reply_issdm-$MDS ops-per-second >> replyt_issdm-$MDS
-parse_replyl.py reply_issdm-$MDS latency >> replyl_issdm-$MDS
