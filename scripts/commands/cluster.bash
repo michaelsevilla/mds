f2ffeaa4f701d2d2c1929b8faca2d5cfdb3f25e4
michaelsevilla <mikesevilla3@gmail.com>
2014-12-17 13:44:31 -0800

    Update cluster.bash

--- scripts/commands/cluster.bash
+++ scripts/commands/cluster.bash
@@ -15,8 +15,9 @@ sudo ../../cleanup.sh; sudo OSD=3 MDS=3 MON=1 ./vstart.sh -l -n; sudo ./ceph -c 
   export CLIENT=
   (sudo ceph-fuse /mnt/cephfs -d) > /mnt/vol2/msevilla/ceph-logs/client/client$CLIENT 2>&1 &
   sudo chown -R msevilla:msevilla /mnt/cephfs
-  /user/msevilla/programs/mdtest/mdtest -F -C -n 100000 -d /mnt/cephfs/dir-$CLIENT
   ./cleanup-caches.sh
+  /user/msevilla/programs/mdtest/mdtest -F -C -n 100000 -d /mnt/cephfs/dir-$CLIENT > /mnt/vol2/msevilla/ceph-logs/client/job$CLIENT 2>&1 &
+  tail -f /mnt/vol2/msevilla/ceph-logs/client/job$CLIENT  
 @MON: 
   vim config.sh
   ./collect.sh
