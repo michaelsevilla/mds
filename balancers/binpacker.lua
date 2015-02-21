8e38a86fdb1a5bdf89192b66a0c335da568191b5
Michael Sevilla <mikesevilla3@gmail.com>
2015-02-21 01:30:04 -0800

    Implemented 4 types of calculating how much to migrate to fulfill the target.

--- balancers/binpacker.lua
+++ balancers/binpacker.lua
@@ -1,72 +1,87 @@
 -- Send off the fragments with the most load, first
 function send_until_target(debug, target, dirfrags)
   sum = 0
-  send = ""
-  mds1 = {}
+  send_dfs = {}
+  nextmin = {-1, 2147483648}
   for i=1,#dirfrags do
-    sum = sum + dirfrags[i]
-    if sum <= target then table.insert(mds1, i)
-    else sum = sum - dirfrags[i] end
+    sum = sum + dirfrags[i][2]
+    if sum <= target then table.insert(send_dfs, dirfrags[i])
+    else 
+      sum = sum - dirfrags[i][2] 
+      if dirfrags[i][2] < nextmin[2] then nextmin = dirfrags[i] end
+    end
   end
-  mds1_sum = 0
-  for i=1,#mds1 do mds1_sum = mds1_sum + dirfrags[mds1[i]] end
-  return math.abs(mds1_sum-target), mds1
+  return send_dfs, nextmin
 end
 
 function small_first(debug, target, dirfrags)
-  d = 9
-  s = {1, 2}
-  --table.sort(dirfrags)
-  --d, s = send_until_target(debug, target, dirfrags)
-  return d, s
+  table.sort(dirfrags, function(a,b) return a[2] < b[2] end )
+  s, n = send_until_target(debug, target, dirfrags)
+  return s
+end
+function small_first_plus1(debug, target, dirfrags)
+  table.sort(dirfrags, function(a,b) return a[2] < b[2] end )
+  s, n = send_until_target(debug, target, dirfrags)
+  table.insert(s, n)
+  return s
 end
 
 function big_first(debug, target, dirfrags)
---  table.sort(dirfrags, function(a,b) return a>b end)
---  d, s = send_until_target(debug, target, dirfrags)
---  for i=1,#s do s[i] = (#dirfrags + 1) - s[i] end
---  return d, s
-  x = {1, 2, 3, 4, 5}
-  return 3, x
+  table.sort(dirfrags, function(a,b) return a[2] > b[2] end )
+  s, n = send_until_target(debug, target, dirfrags)
+  return s
 end
 function big_first_plus1(debug, target, dirfrags)
   -- want to add the next smallest one, since we are up against the barrier
   -- if we hadn't added it already, then we didn't go all the way to the threshold
+  table.sort(dirfrags, function(a,b) return a[2] > b[2] end )
+  s, n = send_until_target(debug, target, dirfrags)
+  table.insert(s, n)
+  return s
 end
 
 -- input:  @target:   STRING how much load we went to offload
 --         @dirfrags: STRING the loads for the directory fragments
-strategies = {big_first, small_first}
+strategies = {small_first, big_first, small_first_plus1, big_first_plus1}
 function pack(debug, target, ...)
   f = io.open(debug, "w")
   io.output(f)
-  io.write(string.format("  [Lua5.2] using %d binpacking strategies to figure out how to send load=%f.\n", 
+  io.write(string.format("  [Lua5.2] using %d binpacking strategies, trying to send load=%f.\n", 
           #strategies, target))
 
-  dirfrags = ipairs{...}
-  --for k,v in ipairs{...} do table.insert(dirfrags, v) end
-  --io.write("  [Lua5.2] print dirfrags: ")
-  --for i=1,#dirfrags do io.write(string.format(" %f", dirfrags[i])) end
+  dirfrags = {}
+  io.write("  [Lua5.2] print dirfrags: ")
+  for k,v in ipairs{...} do 
+    dirfrags[k] = {k, v}
+    io.write(string.format(" df[%d]=%f", dirfrags[k][1], dirfrags[k][2]))
+  end
   io.write("\n")
 
+
   distance = 2147483648
-  export = ""
+  send = {}
   function dofunction(f) return f(debug, target, dirfrags) end
   for i=1,#strategies do 
-    d, e = dofunction(strategies[i]) 
-    table.sort(e)
+    s = dofunction(strategies[i]) 
+
+    sum = 0
+    for i=1,#s do sum = sum + s[i][2] end
+    d = math.abs(sum-target)
     io.write(string.format("  [Lua5.2]  strategy %d: distance=%f, export_dirfrags=", 
              i, d))
-    for j=1,#e do io.write(string.format(" %d", e[j])) end
+    for k,v in ipairs(s) do io.write(string.format(" %d", v[1])) end
     io.write("\n")
     if d < distance then
       distance = d
-      export = e
+      send = s
     end
   end
 
+  ret = ""
+  for k,v in ipairs(send) do ret = ret.." "..v[1] end
+
   io.close(f)
-  return export
+  return ret
 end
 
 
