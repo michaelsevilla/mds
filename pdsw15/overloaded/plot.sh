ffd0ebc9ac66a290a3eed27e3e503fe4284d0ee9
Michael Sevilla <mikesevilla3@gmail.com>
2015-05-27 08:57:56 -0700

    Combine latencies/nrequests into one file; print previous value; reorganize PDSW experiments

---
+++ pdsw15/overloaded/plot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+WORKDIR=`pwd`
+ROOTDIR=`dirname $WORKDIR`
+SCRIPTS=`dirname $ROOTDIR`"/scripts"
+TOOLS=`dirname $ROOTDIR`"/tools"
+$TOOLS/lttng-latency.py -d auto-20150526-111048/ust/uid/1001/64-bit/ -o latencies.dat
+$TOOLS/tailplot/tailplot latencies.dat -x 1 --field-format=1,date,HH:mm:ss --x-format=date,HH:mm:ss -s 2,3,4 -f numrequests,latency,avg --y2=2 &
+
